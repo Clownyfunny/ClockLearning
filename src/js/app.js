50376f076a8a97e6dd0ef8484c5375e4b79caea1
pierre <pierre@pierre.pierre>
2014-11-24 18:48:19 +0100

    Some modification again

--- src/js/app.js
+++ src/js/app.js
@@ -3,20 +3,30 @@ var app = angular.module('ClockLearning', ['ngRoute']);
 app.config(['$routeProvider', function($routeProvider) {
     $routeProvider
     .when('/', {
-        templateUrl: 'partials/home.html',
+        templateUrl: 'src/partials/home.html',
         controller : 'startCtrl'
     })
     .when('/game', {
-        templateUrl: 'partials/clock.html',
+        templateUrl: 'src/partials/clock.html',
         controller : 'gameCtrl'
     })
     .when('/credits', {
-        templateUrl: 'partials/credits.html',
+        templateUrl: 'src/partials/credits.html',
         controller : 'creditCtrl'
     })
     .when('/settings', {
-        templateUrl: 'partials/settings.html',
+        templateUrl: 'src/partials/settings.html',
         controller : 'settingsCtrl'
     })
     .otherwise({ redirectTo: '/' });
 }]);
+
+app.controller('startCtrl', ['$scope', function($scope) {
+}]);
+app.controller('gameCtrl', ['$scope', function($scope) {
+}]);
+app.controller('settingsCtrl', ['$scope', function($scope) {
+}]);
+app.controller('creditCtrl', ['$scope', function($scope) {
+}]);
+
